fdb3035cfe72a5976ac4d8355f6459347e79e0ac
Cassiano-Behemonth <oliveira.cassiano@escola.pr.gov.br>
2025-09-17 09:44:52 -0300

    Modelos de dados

---
+++ app/src/main/java/com/example/mygamelist/Models.kt
@@ -0,0 +1,20 @@
+package com.example.mygamelist
+
+data class Game(
+    val name: String,
+    val description: String,
+    val achievementsCompleted: Int = 0,
+    val totalAchievements: Int = 0,
+    val completionPercentage: Int = 0,
+    val status: GameStatus = GameStatus.IN_PROGRESS
+)
+
+enum class GameStatus {
+    IN_PROGRESS,
+    COMPLETED
+}
+
+data class User(
+    val username: String,
+    val password: String
+)
